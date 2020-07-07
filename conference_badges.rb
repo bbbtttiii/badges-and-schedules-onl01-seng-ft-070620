bb9adbfe2ddc388da1ea0d10f1ae806c5a11ec04
Ben Iburg <ben.iburg@gmail.com>
2020-07-07 19:03:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,17 +13,17 @@ def batch_badge_creator(names)
   end
 end
 
-def assign_rooms(names)
-  names.each_with_index.map do |person, index| 
-    "Hello, #{person}! You'll be assigned to room #{index + 1}!"
-  end
-end
+# def assign_rooms(names)
+#   names.each_with_index.map do |person, index| 
+#     "Hello, #{person}! You'll be assigned to room #{index + 1}!"
+#   end
+# end
 
-def printer(names)
-  batch_badge_creator(names).each do |message|
-    puts message
-  end
-  assign_rooms(names).each do |message|
-    puts message
-  end
-end
+# def printer(names)
+#   batch_badge_creator(names).each do |message|
+#     puts message
+#   end
+#   assign_rooms(names).each do |message|
+#     puts message
+#   end
+# end
